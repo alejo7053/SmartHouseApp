ae4a26c3f88a106395344e3fbd8760b0d17f1060
alejo7053 <alejo7053@gmail.com>
2018-09-07 19:54:17 -0400

    fix public view

---
+++ resources/views/view/public.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="col-md-4 container-fluid text-center">
+    <h1>SmartHouse<h1><br>
+
+    <h4><a href="#">Solicitar registro</a><br>
+    <a href="#">Contactenos</a><br></h4>
+    <h5>Usuarios totales: {{ $nVar }}</h5><br>
+    <h5>Desde: Agosto 2018</h5>
+</div>
+@stop
