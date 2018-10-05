4a72dd6d1f2c3ea103fafdf3b4fdbb464336803d
alejo7053 <alejo7053@gmail.com>
2018-10-05 16:43:23 -0400

    sort index devices

--- resources/views/device/index.blade.php
+++ resources/views/device/index.blade.php
@@ -64,7 +64,7 @@
         @endif
         @if(Auth::user()->role == "userRoom")
         @foreach($user->rooms as $room)
-          @foreach($room->devices as $device)
+          @foreach($room->devices->sortByDesc('role') as $device)
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-header" id="t{{$device->id}}">{{ $device->description }}</div>
